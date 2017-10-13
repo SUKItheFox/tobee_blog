a9a72e5ab74399f6c3b07dc011760683b722943a
Mari Michelson <mari.michelson@gmail.com>
2017-10-13 16:54:41 +0300

    Add Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
- resources :posts do
+  resources :posts do
   	resources :comments
   end
-  
- root "posts#index"
+  	
+  root "posts#index"
+
+  get '/about', to: 'pages#about' 
 end
