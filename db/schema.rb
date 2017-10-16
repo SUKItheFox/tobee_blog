ccea7891c5d02cd34cefe44533a82136665989f6
Mari Michelson <mari.michelson@gmail.com>
2017-10-16 20:17:20 +0300

    Fixing CKeditor for main page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014193304) do
+ActiveRecord::Schema.define(version: 20171016150537) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string "data_file_name", null: false
