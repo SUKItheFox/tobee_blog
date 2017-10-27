7e673f96939ac400271a6ebaca8614c5a96d7c11
Mari Michelson <mari.michelson@gmail.com>
2017-10-27 21:22:15 +0300

    Updated assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -20,8 +20,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 #Line for navigation.js
 Rails.application.config.assets.precompile += %w( navigation.js )
 
-#Line for scroll_top_arrow.js
-Rails.application.config.assets.precompile += %w( scroll_top_arrow.js )
+
 
 
 
