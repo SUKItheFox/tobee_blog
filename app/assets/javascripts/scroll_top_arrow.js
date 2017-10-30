66808ecfde1a29a834556e9c53b9bbd33e4939c3
Mari Michelson <mari.michelson@gmail.com>
2017-10-30 17:46:27 +0200

    removed scroll_top_arrow

--- app/assets/javascripts/scroll_top_arrow.js
+++ app/assets/javascripts/scroll_top_arrow.js
@@ -1,4 +1,4 @@
-// When the user scrolls down 20px from the top of the document, show the button
+/*// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
