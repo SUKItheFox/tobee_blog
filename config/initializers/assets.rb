9b4a060c4206a099ebb229b050addd9b86c9f0d5
Mari Michelson <mari.michelson@gmail.com>
2017-10-30 19:45:57 +0200

    Update assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,7 +14,28 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
  
+
+Rails.application.config.assets.precompile += %w( scroll_top_arrow.js )
+    #Line for logo.js
+Rails.application.config.assets.precompile += %w( logo.js )
+
+    #Line for navigation.js
+Rails.application.config.assets.precompile += %w( navigation.js )
+
+    #Line for scroll_top_arrow.js
+Rails.application.config.assets.precompile += %w( scroll_top_arrow.js )
 Rails.application.config.assets.precompile += %w( page up.png )
+
+    #line for navBar_fadein.js
+Rails.application.config.assets.precompile += %w( navBar_fadein.js )
+
+    #Line for navBar_dropdown.js
+Rails.application.config.assets.precompile += %w( navBar_dropdown.js )
+
+Rails.application.config.assets.precompile += %w( page up.png )
+Rails.application.config.assets.precompile += %w( favicon-32x32.png )
+Rails.application.config.assets.precompile += %w( left sidebar.png )
+Rails.application.config.assets.precompile += %w( right sidebar.png )
 
 
 
