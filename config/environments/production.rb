a82265c6192202cb3103295da37d9f05f972c8ce
Mari Michelson <mari.michelson@gmail.com>
2017-10-30 21:45:02 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = true
+  config.active_record.dump_schema_after_migration = false
 end
