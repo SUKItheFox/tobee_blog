b0eb915cfe7d1f4b82457389399aaec26d803646
Mari Michelson <mari.michelson@gmail.com>
2017-10-30 20:08:14 +0200

    Added updated styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= depend_on_asset "bootstrap.js"
 
 //= require rails-ujs
 //= require bootstrap
