bc95f51b8b7b46c03a218bbbae2b48444cf655f2
Mari Michelson <mari.michelson@gmail.com>
2017-11-04 11:14:59 +0200

    Update possts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,22 +2,24 @@ class PostsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
-		if params[:category].blank?
-			@posts = Post.all.order('created_at DESC')
-		else
-			@category_id = Category.find_by(name: params[:category]).id
-			@posts = Post.where(category_id: @category_id).order("created_at DESC")
-		end	
-
-		if params[:language_category].blank?
-			@posts = Post.all.order('created_at DESC')
-		else
+		if  params[:language_category].present?
 			@language_category_id = LanguageCategory.find_by(name: params[:language_category]).id
 			@posts = Post.where(language_category_id: @language_category_id).order("created_at DESC")
-		end	
+			
 
-	
+		elsif params[:category].present?
+			@category_id = Category.find_by(name: params[:category]).id  
+			@posts = Post.where(category_id: @category_id).order("created_at DESC")
+			
+
+		else	
+			params[:category].blank? and params[:language_category].blank?
+			@posts = Post.all.order('created_at DESC')			
+			
+		end		
 	end
+
+		
 	
 	def new
 		@post = Post.new
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,6 @@
 class Post < ApplicationRecord
 	belongs_to :category 
 	belongs_to :language_category 
-	validates :category, presence: true
-	validates :language_category, presence: true
 	has_many :comments, dependent: :destroy 
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
