30b6924b955330606bd9f3b1e159ac152a0fe4f7
Mari Michelson <mari.michelson@gmail.com>
2017-12-17 01:32:44 +0200

    Add view files to fforumposts and abilities

--- app/controllers/forumposts_controller.rb
+++ app/controllers/forumposts_controller.rb
@@ -1,4 +1,52 @@
 class ForumpostsController < ApplicationController
+	before_action :find_forumpost, only: [:show, :edit, :update, :destroy]
+
+
 	def index
+	end	
+
+	def show
+		@forumpost = Forumpost.find(params[:id])
+	end
+
+	def new
+		@forumpost = Forumpost.new
+	end	 
+
+	def create
+		@forumpost = Forumpost.new(forumpost_params)
+
+		if @forumpost.save
+			redirect_to @forumpost
+		else
+			render 'new'
+		end	
+	end		
+
+	def edit
+	end 
+	
+	def update
+		if @forumpost.update(forumpost_params)
+			redirect_to @forumpost
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@forumpost.destroy
+		redirect_to forum_path
+	end
+
+
+	private
+
+	def find_forumpost
+		@forumpost = Forumpost.find(params[:id])
+	end	
+
+	def forumpost_params
+		params.require(:forumpost).permit(:title, :content)
 	end	
 end
