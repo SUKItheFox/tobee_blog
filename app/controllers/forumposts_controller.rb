f4b44c89fc497d00693cc83c3ad9f9b2e5fee939
Mari Michelson <mari.michelson@gmail.com>
2017-12-17 02:19:24 +0200

    Add authentication to forumposts

--- app/controllers/forumposts_controller.rb
+++ app/controllers/forumposts_controller.rb
@@ -1,5 +1,6 @@
 class ForumpostsController < ApplicationController
 	before_action :find_forumpost, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
 
 	def index
