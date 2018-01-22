13f56de4de805066f63207ca627092e381b7f03c
Suki <31817823+SUKItheFox@users.noreply.github.com>
2018-01-22 22:00:05 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
-  
+  before_action :check_authorization, only: [:edit, :update]
   before_action :set_user, only: [:edit, :update]
 
   attr_accessor  :email, :username
